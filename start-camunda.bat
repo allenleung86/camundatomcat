1b124b4ef3d9f15dd9511ed91c043daded0b23d3
allenleung86 <londalonda@qq.com>
2019-05-23 10:50:20 +0800

    初始提交

---
+++ start-camunda.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set "CATALINA_BASE=%CD%\server\apache-tomcat-9.0.12"
+set "CATALINA_HOME=%CD%\server\apache-tomcat-9.0.12"
+set "CATALINA_TMPDIR=%CD%\server\apache-tomcat-9.0.12\temp"
+
+echo "starting camunda BPM platform 7.10.0 on Apache Tomcat 9.0.12"
+
+cd server\apache-tomcat-9.0.12\bin\
+start startup.bat
+
+ping -n 5 localhost > NULL
+start http://localhost:8080/camunda-welcome/index.html
+ 
